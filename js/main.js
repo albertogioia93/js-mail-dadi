848747c62aea4c278431ff85d9dfd44fb3837a36
albertogioia93 <albertogioia@hotmail.it>
2023-02-24 16:04:03 +0100

    esercizio email

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 'use strict';
 
 // chiedo all'utente la sua email
-// const emailUtente = prompt('Inserisci la tua email');
-// console.log(emailUtente);
+const emailUtente = prompt('Inserisci la tua email');
 
 // creo una lista di email
 const listaEmail = ["index@gmail.it", "css@gmail.it", "js@gmail.it"];
 
-// console.log(listaEmail[0]);
-// console.log(listaEmail[0]);
-// console.log(listaEmail[1]);
-// console.log(listaEmail[2]);
-
+let accettazione = false;
 
 // uso un ciclo per scorrere la lista di email
-for (let i = 0; i < listaEmail.length; i++); {
-    console.log(listaEmail[i]);
+for (let i = 0; i < listaEmail.length; i++){
+    const email = listaEmail[i];
+    if (email === emailUtente){
+        accettazione = true;
+    }
 }
 
-
-// if i === emailUtente
+// stampo il risultato dell'accettazione
+if (accettazione === true){
+    alert ('email in lista');
+} else {
+    alert ('email non in lista');
+}
