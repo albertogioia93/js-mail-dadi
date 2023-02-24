bf3ecc78f5355fa36ada30b514849f31965a3ac2
albertogioia93 <albertogioia@hotmail.it>
2023-02-24 15:26:00 +0100

    non capisco perchè non mi fa stampare la i

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-alert('ciao');
+// chiedo all'utente la sua email
+// const emailUtente = prompt('Inserisci la tua email');
+// console.log(emailUtente);
+
+// creo una lista di email
+const listaEmail = ["index@gmail.it", "css@gmail.it", "js@gmail.it"];
+
+// console.log(listaEmail[0]);
+// console.log(listaEmail[0]);
+// console.log(listaEmail[1]);
+// console.log(listaEmail[2]);
+
+
+// uso un ciclo per scorrere la lista di email
+for (let i = 0; i < listaEmail.length; i++); {
+    console.log(listaEmail[i]);
+}
+
+
+// if i === emailUtente
