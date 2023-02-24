892ed009473fe59bba7573c3491b78a71c405941
albertogioia93 <albertogioia@hotmail.it>
2023-02-24 17:09:18 +0100

    prova di quello che dovrebbe accadere

--- esercizio dadi/js/main.js
+++ esercizio dadi/js/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
 
-let dado = Math.floor(Math.random() * 6) + 1;
-console.log(dado);
+const dadoGiocatore = Math.floor(Math.random() * 6) + 1;
+const dadoComputer = Math.floor(Math.random() * 6) + 1;
+// console.log(dadoGiocatore);
+// console.log(dadoComputer);
+
+
+if (dadoGiocatore > dadoComputer) {
+    console.log('Hai vinto');
+} else if (dadoGiocatore < dadoComputer) {
+    console.log('Hai perso');
+} else {
+    console.log('Pareggio');
+}
